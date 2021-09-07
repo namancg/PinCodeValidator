b550545ec1869936ded14623df190f41310d1c9a
namancg <naman.c@perfios.com>
2021-09-07 19:55:11 +0530

    ADDED: method to check if pincode is 6digits

--- src/com/bridgelabz/pinccode/PinCode.java
+++ src/com/bridgelabz/pinccode/PinCode.java
@@ -1,10 +1,30 @@
 package com.bridgelabz.pinccode;
-
+import java.util.*;
+import java.util.regex.*;
 public class PinCode {
 
 	public static void main(String[] args) {
-		System.out.println("PIN CODE VALIDATION");
+			Scanner sc= new Scanner(System.in);
+			System.out.println("ENTER THE PINCODE TO BE CHECKED");
+			String pincode= sc.next();
+			checkValidFirstName(pincode);
+		}
+		public static void checkValidFirstName(String pincode) 
+		{
+			String regexCheck= "[0-9]{6}";
+			Pattern pattern = Pattern.compile(regexCheck);
+			Matcher obj= pattern.matcher(pincode);
+			if(obj.matches()==true)
+			{
+			System.out.println("THIS IS A VALID PINCODE");
+				
+			}
+			else
+				System.out.println("NOT A VALID PINCODE");
+			
+		}
+
 
 	}
 
-}
+
